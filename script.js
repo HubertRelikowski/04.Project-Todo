f0ee084f8f157275e4a2f4167b5f275dfb957c53
Hubert Relikowski <relikowski.hubert@gmail.com>
2024-01-17 13:52:50 +0100

    Add remove task function

--- script.js
+++ script.js
@@ -11,6 +11,21 @@
             },
         ];
 
+        const addNewTask = (newTaskContent) => {
+
+            tasks.push({
+                content: newTaskContent,
+            });
+    
+            render();
+    
+        };
+    
+        const removeTask = (taskIndex ) => {
+            tasks.splice(taskIndex, 1);
+            render();
+        }
+
     
     const render = () => {
         let htmlString = "";
@@ -18,23 +33,24 @@
         for(const task of tasks) {
             htmlString += `
                 <li ${task.done ? "style=\"text-decoration:line-through\"" : ""  }>
+                    <button class="js-remove">usuń</button>
                     ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            })
+        })
     };
 
-    const addNewTask = (newTaskContent) => {
-
-        tasks.push({
-            content: newTaskContent,
-        });
-
-        render();
-
-    };
+    
 
     const onFormSubmit = (event) => {
     
